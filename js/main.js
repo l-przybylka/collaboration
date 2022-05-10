5951c6e63d235afafb2cb3f2c41c818f166ab83e
Jonathan Harvey <jonathanharvey22@gmail.com>
2022-05-10 12:19:15 +0200

    added some fruit classes and extended them using the four pillars of OOP

--- js/main.js
+++ js/main.js
@@ -1,8 +1,66 @@
-fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-            console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      })
+// fetch(url)
+//       .then(res => res.json()) // parse response as JSON
+//       .then(data => {
+//             console.log(data)
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       })
+
+// Main class of Fruit
+
+class Fruit{
+  constructor(name, color, flavor){
+    this._name = name
+    this.color = color
+    this.flavor = flavor
+  }
+  eat(){
+    console.log(`this ${this.color} ${this.name} tastes ${this.flavor}`)
+  }
+  get name(){
+    return this._name
+  }
+}
+
+class Berries extends Fruit{
+  constructor(name, color, flavor, dried){
+    super(name, color, flavor)
+    this.dried = dried
+  }
+  ferment(){
+    console.log(`a ${this.flavor} wine was made`)
+  }
+  get fruitFlavor(){
+    return this.flavor
+  }
+}
+
+class Drupes extends Fruit{
+  constructor(name, color, flavor, needsPittingToEat){
+    super(name, color, flavor)
+    this.needsPittingToEat = needsPittingToEat
+  }
+  eat(){
+    if(this.needsPittingToEat){
+      console.log(`this ${this.name} needs to be pitted first`)
+    }
+    else{
+      super.eat()
+    }
+  }
+}
+
+class Pomes extends Fruit{
+  constructor(name, color, flavor, ediblePeel){
+    super(name, color, flavor)
+    this.ediblePeel = ediblePeel
+  }
+  cutSlice(){
+    console.log(`You cut off a slice of this ${this.name} and ate it`)
+  }
+}
+let apricot = new Drupes("Apricot", "orange", "sweet", false)
+apricot.eat()
+let strawberry = new Berries("strawberry", "red", "sweet", false)
+console.log(strawberry.fruitFlavor)
